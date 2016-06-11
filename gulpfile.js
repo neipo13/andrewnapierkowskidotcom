7cfdee9cc0fa66424a3fdd28d2623983babb3ec5
Noel Berry <thenberry@gmail.com>
2016-06-11 15:19:31 -0700

    fixed a bunch of the gulp stuff so it gets all works files

--- gulpfile.js
+++ gulpfile.js
@@ -11,67 +11,74 @@ var gulp = require('gulp'),
 	path = require("path"),
 	fs = require("fs");
 var json = {};
+var source = './source/';
+var bin = './site/';
 
 gulp.task("css", function()
 {
-	gulp.src("source/css/*")
+	return gulp.src(source + "css/*")
 	.pipe(cleanCSS())
-	.pipe(gulp.dest("site/css/"));
+	.pipe(gulp.dest(bin + "css/"));
 })
 
 gulp.task("images", function()
 {
-	gulp.src(["source/images/*", "source/works/*/*.png"])
-	.pipe(gulp.dest("site/img"));
+	return gulp.src([source + "images/*", source + "works/*/*.png"])
+	.pipe(gulp.dest(bin + "img"));
 });
 
 gulp.task("image-compress", function()
 {
-	gulp.src(["source/images/*", "source/works/*/*.png"])
+	return gulp.src([source + "images/*", source + "works/*/*.png"])
 	.pipe(smushit({ verbose: true}))
-	.pipe(gulp.dest("site/img"));
+	.pipe(gulp.dest(bin + "img"));
 })
 
 gulp.task("works", function()
 {
 	json = {};
-	gulp.src('./source/works/*/index.json')
-	.pipe(function() 
+	return gulp.src(source + 'works/*/*{.json,.html}')
+	.pipe(through.obj(function(file, encoding, callback) 
 	{
-		return through.obj(function(file, encoding, callback) 
+		var info = path.parse(file.path);
+		var working = file.path.split(path.sep);
+		var id = working[working.length - 2];
+
+		if (json[id] == undefined)
+			json[id] = {};
+		var work = json[id];
+		work.id = id;
+
+		if (info.ext == ".json")
 		{
 			var data = JSON.parse(file.contents.toString('utf-8'));
-			var base = path.dirname(file.path);
+			for (var k in data)
+				work[k] = data[k];
+		}
+		else if (info.ext == ".html")
+		{
+			work[info.name] = file.contents.toString('utf-8');
+		}
 
-			// set ID based on the directory name
-			var working = base.split(path.sep);
-			data.id = working[working.length - 1];
-
-			// get content files and add them as attributes to the data
-			var files = fs.readdirSync(base);
-			for (var i = 0; i < files.length; i ++)
-			{
-				var full = path.join(base, files[i]);
-				var info = path.parse(full);
-				if (info.ext == ".html")
-					data[info.name] = fs.readFileSync(full).toString('utf-8');
-			}
-
-			// add to global json object
-			if (json[data.type] == undefined)
-				json[data.type] = [];
-			json[data.type].push(data);
-			json[data.type].sort(function(a, b) { return Math.sign(b.order - a.order); });
-			
-			return callback(null, file);
-		});
-	}());
+		callback();
+	}));
 });
 
 gulp.task("template", function()
 {
-	gulp.src('source/templates/index.mustache')
-	.pipe(mustache(json))
+	var sorted = {};
+	for (var k in json)
+	{
+		var type = json[k].type;
+		if (sorted[type] == undefined)
+			sorted[type] = [];
+		sorted[type].push(json[k]);
+	}
+	for  (var k in sorted)
+		sorted[k].sort(function(a, b) { return Math.sign(b.order - a.order); });
+
+	return gulp.src(source + 'templates/index.mustache')
+	.pipe(mustache(sorted))
 	.pipe(rename('index.html'))
 	.pipe(htmlmin({collapseWhitespace: true }))
 	.pipe(gulp.dest('site'));
@@ -79,9 +86,9 @@ gulp.task("template", function()
 
 gulp.task("scripts", function()
 {
-	gulp.src("source/scripts/*.js")
+	return gulp.src(source + "scripts/*.js")
 	.pipe(uglify().on("error", gutil.log))
-	.pipe(gulp.dest("site/js"));
+	.pipe(gulp.dest(bin + "js"));
 });
 
 gulp.task('clean', function() 
